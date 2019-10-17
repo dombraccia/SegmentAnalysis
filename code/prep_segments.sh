34ff8659608066046c325bb8a3f5a844d85bc947
dombraccia <dbraccia@umd.edu>
2019-10-16 20:24:48 -0400

    updating prep_segments.sh

--- code/prep_segments.sh
+++ code/prep_segments.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -J prep_seg # Job name 
-#SBATCH -o /dev/null # suppress output file
-#SBATCH -e /dev/null # suppress error file
+#SBATCH -o prep_segments_%j.o # output file
+#SBATCH -e prep_segments_%j.e # error file
 #SBATCH --mail-user=dbraccia@terpmail.umd.edu # Email for job info
 #SBATCH --mail-type=fail,end # Get email for begin, end, and fail
-#SBATCH --time=18:00:00                                                   
-#SBATCH --qos=throughput 
-#SBATCH --mem=36gb
+#SBATCH --time=21-00:00:00                                                   
+#SBATCH --qos=xlarge 
+#SBATCH --mem=495gb
 
 # Preparing metadata for parsing and analysis in python / GenomicRanges (R)
 
-# go from "gfa" file to "fasta" file using `gfa2fa` from `gfatools`
-time fs/cbcb-lab/hcorrada/SGVFinder/SegmentsAnalysis/external/gfatools/gfatools \
-    gfa2bed -m 'path/to/gfa/file.gfa1' > 'path/to/BED/file.bed' 
+# go from "gfa" file to "bed" file using `gfa2fa` from `gfatools`
+time ../external/gfatools/gfatools \
+    gfa2bed -m ../data/scg_segments_k99.gfa1 > ../data/scg_segments_k99.bed
