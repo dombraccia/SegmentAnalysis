04002ba33c25d8ad3a10ab7e709f051cd4a5d3ec
dombraccia <dbraccia@umd.edu>
2019-12-08 18:47:55 -0500

    general housekeeping (deleted outdated scripts)

--- code/create_dicts/getplines.py
+++
@@ -1,8 +0,0 @@
-f_out= open("plines.txt","w")
-
-with open('scg_segments_k99.gfa1') as segk99:
-    for line in segk99:
-        if line[0] == 'P':
-            f_out.write("%s \n" %line)
-f_out.close()
-segk99.close()
